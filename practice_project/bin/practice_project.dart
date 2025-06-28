87b0d58319a4302693256ee5d3ff95cd3dc5dbce
Jahid Ahmed <ajahid97@gmail.com>
2025-06-28 12:39:45 +0600

    updates

--- practice_project/bin/practice_project.dart
+++ practice_project/bin/practice_project.dart
@@ -1,61 +1,14 @@
-import 'package:args/args.dart';
+void main(){
 
-const String version = '0.0.1';
+  var x = 20;
+  var y = 30;
+  var z = x + y;
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag(
-      'version',
-      negatable: false,
-      help: 'Print the tool version.',
-    );
-}
+  var name = 'My Name is Jahid Ahmed!';
+  var countryList = ['Dhaka', 'Chattogram', 'Sylhet', 'Rangpur', 'Khulna', 'Rajshahi', 'Mymansingh', 'Barishal'];
 
-void printUsage(ArgParser argParser) {
-  print('Usage: dart practice_project.dart <flags> [arguments]');
-  print(argParser.usage);
-}
+  print("My Home Town is \'${countryList[1]}\' ");
 
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
-
-    // Process the parsed arguments.
-    if (results.wasParsed('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.wasParsed('version')) {
-      print('practice_project version: $version');
-      return;
-    }
-    if (results.wasParsed('verbose')) {
-      verbose = true;
-    }
-
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
-  }
+  print(z);
+  print(name);
 }
